1d6b6857afe76e17e6a4bdedbe585ad42f6cad3b
Martin Vicente <panchoeltaco19@gmail.com>
2025-05-14 19:47:35 -0300

    Agrega nueva funcionalidad

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  print("Segundo practico");
+  //runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
